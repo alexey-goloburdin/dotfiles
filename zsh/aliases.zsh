81df1156604fae1774afa6e164c8573933b5e862
Alexey Goloburdin <sterx@rl6.ru>
2025-04-03 13:08:38 +0300

    Add pwgen zsh alias — password generate. Comment pbcopy zsh alias (may be temporary)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias n=nvim
 alias open=explorer.exe
-alias pbcopy="win32yank.exe -i"
+alias pwgen="LC_ALL=C tr -dc 'A-Za-z0-9@#%^&*()_+=-{}[]:;<>,.?/' < /dev/urandom | head -c 20 | xargs echo"
+#alias pbcopy="win32yank.exe -i"
 
