7bc3604597e814afcb94a370ceec5d8192f8fbab
Alexey Goloburdin <sterx@rl6.ru>
2025-04-10 19:49:26 +0300

    for wl-copy working

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,10 @@
 alias n=nvim
 alias open=explorer.exe
-alias pwgen="LC_ALL=C tr -dc 'A-Za-z0-9@#%^&*()_+=-{}[]:;<>,.?/' < /dev/urandom | head -c 20 | xargs echo"
 #alias pbcopy="win32yank.exe -i"
+
+# Password generator
+pwgen() {
+  local len=${1:-20}
+  LC_ALL=C tr -dc 'A-Za-z0-9@#%^&*()_+=-{}[]:;<>,.?/' < /dev/urandom | head -c "$len" | xargs echo
+}
 
